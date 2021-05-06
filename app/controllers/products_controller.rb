6e0b9a603d98874589b1375da1f62257c32c8263
Shinan Liao <shinanliao1@gmail.com>
2021-05-06 09:34:15 -0700

    making routes to display all products and last product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,11 @@
 class ProductsController < ApplicationController
+  def show_all
+    products = Product.all
+    render json: products.as_json
+  end
+
+  def show_one
+    bed_product = Product.last
+    render json: bed_product.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/all_products", controller: "products", action: "show_all"
+  get "/one_product", controller: "products", action: "show_one"
 end
