533c24a5e4bd6a27d98272d745262f62e6dd54e2
Shinan Liao <shinanliao1@gmail.com>
2021-05-10 13:55:21 -0700

    adds restful routes for patching and destroying

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,8 +4,35 @@ class ProductsController < ApplicationController
     render json: products.as_json
   end
 
+  def create 
+    product = Product.new(
+    name: params[:name],
+    price: params[:price],
+    image_url: params[:image_url],
+    description: params[:description]
+  )
+    product.save
+    render json: product.as_json
+  end
+
   def show
     product = Product.find(params[:id])
     render json: product.as_json
+  end
+
+  def update
+    product = Product.find(params[:id])
+    product.name = params[:name] || product.name
+    product.price = params[:price] || product.price
+    product.image_url = params[:image_url] || product.image_url
+    product.description = params[:description] || product.description
+    product.save
+    render json: product.as_json
+  end
+
+  def destroy
+    product = Product.find(params[:id])
+    product.destroy
+    render json: {message: "Goodbye forever!"}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/products", controller: "products", action: "index"
+  post "/products", controller: "products", action: "create"
   get "/products/:id", controller: "products", action: "show"
+  patch "/products/:id", controller: "products", action: "update"
+  delete "/products/:id", controller: "products", action: "destroy"
 end
