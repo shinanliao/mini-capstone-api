3166eca46f37a72477352c8b669b9eb60861392b
Shinan Liao <shinanliao1@gmail.com>
2021-05-11 12:35:09 -0700

    adding class methods

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -17,7 +17,7 @@ class ProductsController < ApplicationController
 
   def show
     product = Product.find(params[:id])
-    render json: product.as_json
+    render json: product.as_json(methods: [:is_discounted?, :tax, :total])
   end
 
   def update
