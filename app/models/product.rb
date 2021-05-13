c70a803f755ce10c6b18033cc954f6cb0a74ad0b
Shinan Liao <shinanliao1@gmail.com>
2021-05-13 12:20:47 -0700

    adds validators to products model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,10 @@
 class Product < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+  validates :price, numericality: { greater_than: 0 }
+  validates :description, length: { in: 10..500 }
+  validates :inventory, numericality: { greater_than_or_equal_to: 0 }
+  validates_format_of :image_url, :with => %r{\.(png|jpg|jpeg)$}i, :multiline => true
+  
   def is_discounted?
     price < 10
   end
